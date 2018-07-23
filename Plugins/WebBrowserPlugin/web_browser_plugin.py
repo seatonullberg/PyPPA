b37fcffd2a90ac8a4492da47740adfd603594d85
seatonullberg <seatonullberg@gmail.com>
2018-07-22 23:00:48 -0400

    netflix support

--- Plugins/WebBrowserPlugin/web_browser_plugin.py
+++ Plugins/WebBrowserPlugin/web_browser_plugin.py
@@ -1,6 +1,5 @@
 from base_plugin import BasePlugin
-from multiprocessing import Process
-import time
+import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,17 +10,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 class WebBrowserPlugin(BasePlugin):
 
     def __init__(self):
-        # remember to place the single word spelling last to avoid 'best spelling' issue
         self.name = 'WebBrowserPlugin'
-        # structure for now
-        # search_google: do a google search for a given keyword or phrase
-        # -- build Beta for enhanced audio control
-        # search_netflix: open and login to netflix
-        # -- build beta for enhanced audio control
-        # search_youtube: do a youtube search to find videos related to a given keyword
-        # -- build beta for enhanced audio support
+        # remember to place the single word spelling last to avoid 'best spelling' issue
         self.COMMAND_HOOK_DICT = {'search_google': ['search google for', 'search google', 'search for', 'google'],
-                                  'search_netflix': ['search netflix for ', 'search netflix', 'open netflix'],
+                                  'search_netflix': ['search netflix for', 'search netflix', 'open netflix'],
                                   'search_youtube': ['search youtube for', 'search youtube', 'open youtube']}
         self.MODIFIERS = {'search_google': {},
                           'search_netflix': {},
@@ -49,7 +41,36 @@ class WebBrowserPlugin(BasePlugin):
                              data=driver)
 
     def search_netflix(self):
+        # set environment variables
+        NETFLIX_EMAIL = self.config_obj.environment_variables[self.name]['NETFLIX_EMAIL']
+        NETFLIX_PASSWORD = self.config_obj.environment_variables[self.name]['NETFLIX_PASSWORD']
+        NETFLIX_USER = self.config_obj.environment_variables[self.name]['NETFLIX_USER']
         driver = self._generate_driver()
+        driver.get('https://www.netflix.com/')
+        # move to corner for easy cursor manipulation later
+        driver.set_window_position(x=0, y=0)
+
+        # sign in
+        try:
+            driver.find_element_by_link_text('Sign In').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'login-body')))
+            login_email_element = driver.find_element_by_id('email')
+            login_password_element = driver.find_element_by_id('password')
+            login_email_element.send_keys(NETFLIX_EMAIL)
+            login_password_element.send_keys(NETFLIX_PASSWORD)
+            driver.find_element_by_xpath("*//button[contains(text(),'Sign In')]").click()
+        except selenium.common.exceptions.NoSuchElementException:
+            print('already logged in')
+
+        # select my profile
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'profile-icon')))
+            driver.find_element_by_xpath("//span[text()='{}']".format(NETFLIX_USER)).click()
+        except selenium.common.exceptions.NoSuchElementException:
+            print('already selected account')
+
+        # wait for search to be available
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'searchTab')))
         # send to beta
         self.initialize_beta(name='netflix_search_beta',
                              cmd='search {}'.format(self.command_dict['premodifier']),
@@ -61,31 +82,3 @@ class WebBrowserPlugin(BasePlugin):
         self.initialize_beta(name='youtube_search_beta',
                              cmd='search {}'.format(self.command_dict['premodifier']),
                              data=driver)
-
-    def open_netflix(self):
-        driver = self._generate_driver()
-        driver.get('https://www.netflix.com/')
-        # move to corner for easy cursor manipulation later
-        driver.set_window_position(x=0, y=0)
-        # sign in
-        driver.find_element_by_link_text('Sign In').click()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'login-body')))
-        login_email_element = driver.find_element_by_id('email')
-        login_password_element = driver.find_element_by_id('password')
-        login_email_element.send_keys(NETFLIX_EMAIL)
-        login_password_element.send_keys(NETFLIX_PASSWORD)
-        driver.find_element_by_class_name('btn.login-button.btn-submit.btn-small').click()
-        # select my profile
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'profile-icon')))
-        driver.find_element_by_xpath("//span[text()='{}']".format(NETFLIX_USER)).click()
-        # wait for search to be available
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'searchTab')))
-
-        # initialize the beta plugin for greater control in separate process
-        beta = NetflixSearchBeta('go back')
-        nsb_thread = Process(target=beta.function_handler(args={'driver': driver}), name='netflix_search_beta')
-        while beta.isBlocking:
-            time.sleep(0.1)
-        # after context is released from beta terminate its process
-        nsb_thread.terminate()
-        self.isBlocking = False
