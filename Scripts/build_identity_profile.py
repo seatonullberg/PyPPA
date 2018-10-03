3b66a67bc58d942026581b0e084ddf4c507add0e
seatonullberg <seatonullberg@gmail.com>
2018-10-03 18:49:10 -0400

    moved preprocessing step

--- Scripts/build_identity_profile.py
+++ Scripts/build_identity_profile.py
@@ -11,8 +11,8 @@
 # Directory name will be used as the name of the profile
 # Underscores are interpreted as spaces when constructing the profile: Seaton_Ullberg/ ==> name: Seaton Ullberg
 
-# Preprocess the images and write them to /IdentityProfiles/<dir_name>/faces/
-# Write /IdentityProfiles/<dir_name>/<dir_name>.yaml
+# Preprocess the images and write them to /IdentityProfiles/<dir_name>/images/
+# Write /IdentityProfiles/<dir_name>/profile.yaml
 # -- name: <dir name>
 # -- id: <len(os.listdir(/IdentityProfiles/))>
 
@@ -22,9 +22,58 @@
 # Use model.p in a recognition service which WatcherService will monitor with a thread
 import argparse
 import os
+import cv2
 from utils import identity_profile_utils
-from FacialRecognition import preprocess
 from FacialRecognition import model
+
+
+def preprocess(input_path):
+    """
+    Preprocesses images in input_path for facial recognition
+    :param input_path: (str) absolute path to a directory of facial images
+    """
+    # load cascade file
+    pyppa_dir = os.path.dirname(__file__)
+    pyppa_dir = os.path.dirname(pyppa_dir)
+    casc_path = os.path.join(pyppa_dir, "bin", "frontal_face_cascade.xml")
+    face_cascade = cv2.CascadeClassifier(casc_path)
+
+    # iterate through images
+    for fname in os.listdir(input_path):
+        image_path = os.path.join(input_path, fname)
+        image = cv2.imread(image_path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert to gray scale
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=20,
+            minSize=(100, 100)
+        )
+
+        if len(faces) != 1:  # only accept single face images
+            continue
+
+        for (x, y, w, h) in faces:
+            # crop image around area of face to (100x100)
+            crop = gray[y:y + h, x:x + w]
+            crop = cv2.resize(crop,
+                              dsize=(100, 100),
+                              interpolation=cv2.INTER_CUBIC)
+
+            # TODO: Rotate crop so that the eyes are aligned
+
+            # generate path in /IdentityProfiles/
+            dir_name = os.path.basename(input_path)
+            outdir = os.path.join(pyppa_dir,
+                                  "IdentityProfiles",
+                                  dir_name,
+                                  "images")
+            # make dir if not exists
+            if not os.path.isdir(outdir):
+                os.makedirs(outdir)
+            # save file
+            outfile = os.path.join(outdir, fname)
+            cv2.imwrite(filename=outfile, img=crop)
 
 
 def main(path=None):
@@ -37,7 +86,7 @@ def main(path=None):
         if args.path is None:
             raise argparse.ArgumentError("--path is a required argument")
         path = args.path
-    preprocess.process(path)  # preprocess the images in the supplied path
+    preprocess(path)  # preprocess the images in the supplied path
     name = os.path.basename(path)  # use the supplied directory name as the name of the profile
     profile = identity_profile_utils.new_profile(name)  # make a new profile
     model.build()
